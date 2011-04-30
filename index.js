a2cea97b03824a5bc4be5f30965f997795a2e3ee
James Halliday <mail@substack.net>
2011-04-29 18:14:00 -0700

    crazy slow to initialize with dict.json

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-var fs = require('fs');
-var Lazy = require('lazy');
-
-var offsets = {};
-
-exports.pronounce = function (word, cb) {
-    word = word.toUpperCase();
+var exports = module.exports = function (word, cb) {
+    var xs = pronounce(word).reduce(function (acc, x) {
+        acc[active(x)] = true;
+        return acc;
+    }, {});
     
-    var start = offsets[word[0]];
-    var end = offsets[String.fromCharCode(word[0].charCodeAt(0) + 1)];
-    
-    var s = fs.createReadStream(__dirname + '/data/cmudict.0.7a', {
-        start : start,
-        end : end,
-    });
-    
-    s.once('end', function () {
-        cb(null, found);
-    });
-    
-    var letter = null;
-    var offset = 0;
-    var found = [];
-    
-    Lazy(s).lines.forEach(function (line) {
-        var x = String.fromCharCode(line[0]);
-        if (x.match(/^[A-Z]/i)) {
-            if (letter !== x) { // letter transition
-                if (!offsets[x]) offsets[x] = offset;
-            }
-            
-            var str = line.toString();
-            var w = str.match(/^(\w[\w-']*)/);
-            if (w && w[1] == word) {
-                found.push(str.split(/\s+/).slice(1));
-            }
-            else if (found.length > 0) {
-                s.emit('end');
-            }
-        }
+    return Object.keys(dict).filter(function (w) {
+        var ys = dict[w].map(active);
         
-        offset += line.length + 1;
+        return ys.some(function (y) { return xs[y] });
     });
 };
+exports.rhyme = exports;
+
+var fs = require('fs');
+var dict = exports.dict = JSON.parse(
+    fs.readFileSync(__dirname + '/data/dict.json', 'utf8')
+);
+
+exports.pronounce = function (word) {
+    return dict[word];
+};
+
+function active (ps) {
+    // active rhyming region: slice off the leading consonants
+    var ws = ps.split(' ');
+    for (
+        var i = 0;
+        i < ws.length && ws[i].match(/^[^AEIOU]/i);
+        i++
+    );
+    return ws.slice(i).join(' ');
+}
